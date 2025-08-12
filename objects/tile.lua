8352c7336c2d55d90339c6fe7b51f75b07f9f739
Riley Wrenn <theaithr@gmail.com>
2025-08-12 11:31:16 +1000

    Reworked biome generation again, simplified the process of adding new biomes

--- objects/tile.lua
+++ objects/tile.lua
@@ -2,10 +2,12 @@ local Tile = {}
 Tile.__index = Tile
 
 biomeList = {
-	oceanBiome = {
-		name = "ocean",
+	shallowOceanBiome = {
+		name = "shallowOcean",
 		color = {0.2, 0.5, 1, 1},
-		speedMult = 0.75,
+		speedMult = 0.85,
+		threshold = 0.2,
+		threshold2 = 0.3,
 		states = {
 			{name="empty", weight=1000},
 			{name="combat", weight=200},
@@ -18,6 +20,8 @@ biomeList = {
 		name = "deepOcean",
 		color = {0, 0, 1, 1},
 		speedMult = 0.75,
+		threshold = 0.2,
+		threshold2 = 1,
 		states = {
 			{name="empty", weight=1000},
 			{name="combat", weight=400},
@@ -30,6 +34,8 @@ biomeList = {
 		name = "beach",
 		color = {1, 1, 0, 1},
 		speedMult = 1,
+		threshold = 0.4,
+		threshold2 = 1,
 		states = {
 			{name="empty", weight=1000},
 			{name="combat", weight=0},
@@ -42,6 +48,8 @@ biomeList = {
 		name = "grassland",
 		color = {0, 1, 0, 1},
 		speedMult = 1,
+		threshold = 0.875,
+		threshold2 = 0.5,
 		states = {
 			{name="empty", weight=1000},
 			{name="combat", weight=100},
@@ -54,6 +62,8 @@ biomeList = {
 		name = "forest",
 		color = {0.1, 0.8, 0.1, 1},
 		speedMult = 1,
+		threshold = 0.875,
+		threshold2 = 1,
 		states = {
 			{name="empty", weight=1000},
 			{name="combat", weight=150},
@@ -62,10 +72,12 @@ biomeList = {
 		}
 	},
 
-	tundraBiome = {
-		name = "tundra",
+	mountainBiome = {
+		name = "mountain",
 		color = {1, 1, 1, 1},
 		speedMult = 1,
+		threshold = 1,
+		threshold2 = 1,
 		states = {
 			{name="empty", weight=1500},
 			{name="combat", weight=75},
@@ -84,6 +96,9 @@ function Tile.new(x, y, baseBiome)
     self.height = Game.states.explore.tileSize
     self.x = self.xCoor * self.width
     self.y = self.yCoor * self.height
+
+	self.n = 0
+	self.n2 = 0
 	
 	self.biome = self:rollBiome() or "grassland"
 
@@ -129,35 +144,39 @@ function Tile:clear()
 	Game.stateManager:switch("explore")
 end
 
-local function getNoiseBiome(x, y)
-    -- Lower scale = larger islands
-    local scale = 0.03
-	local scaleGrassland = 0.05
-	local scaleOcean = 0.05
-    local n = love.math.noise(x * scale + Game.noiseSeed, y * scale + Game.noiseSeed)
-	local nGrassland = love.math.noise(x * scaleGrassland + Game.noiseSeed, y * scaleGrassland + Game.noiseSeed)
-	local nOcean = love.math.noise(x * scaleOcean + Game.noiseSeed, y * scaleOcean + Game.noiseSeed)
-    if n < 0.2 then
-        if nOcean < 0.3 then
-			return "deepOcean"
-		elseif nOcean < 1 then
-			return "ocean"
-		end
-    elseif n < 0.4 then
-        return "beach"
-	elseif n < 0.875 then
-		if nGrassland < 0.5 then
-			return "grassland"
-		elseif nGrassland < 1 then
-			return "forest"
-		end
-	elseif n < 1 then
-		return "tundra"
+function Tile:getNoiseBiome(x, y)
+    local scale = 0.025
+	local scale2 = 0.025
+
+    self.n = love.math.noise(x * scale + Game.noiseSeed, y * scale + Game.noiseSeed)
+	self.n2 = love.math.noise(x * scale2 + 2 * Game.noiseSeed, y * scale2 + 2 * Game.noiseSeed)
+
+	local biomes = {}
+    for _, b in pairs(biomeList) do
+        table.insert(biomes, b)
     end
+
+	table.sort(biomes, function(a, b)
+        if a.threshold == b.threshold then
+            return a.threshold2 < b.threshold2
+        else
+            return a.threshold < b.threshold
+        end
+    end)
+
+	for _,b in pairs(biomes) do
+		if self.n < b.threshold then
+			if self.n2 < b.threshold2 then
+				return b.name
+			end
+		end
+	end
+
+return biomes[#biomes].name
 end
 
 function Tile:rollBiome()
-	return getNoiseBiome(self.xCoor, self.yCoor)
+	return self:getNoiseBiome(self.xCoor, self.yCoor)
 end
 
 return Tile
